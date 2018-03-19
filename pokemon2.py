caa09b8d4a9341217adac37e56caf3a88123f169
Unknown <tiaaagosr@gmail.com>
2018-03-19 00:57:59 -0500

    Adding stats.csv and creating battle function
    I changed moved the code for battle similation into a function, which is easy to call now. Next step is to fine tune the return so it fits my goal of computing everything single battle possible! Then, the battles themselves.

--- pokemon2.py
+++ pokemon2.py
@@ -7,6 +7,8 @@ tipo = {}
 tipoGuia = {}
 cnt = 1
 
+# Creates the 'pkmn' dictionary. Each entrance, from '1' to '802' represents a Pokémon. The Dictionary contains the pokémon name, type(s) and stats.
+# Stats are, in order, HP, Attack, Defensa, Sp.Atk, Sp.Def, Speed, Total Stats Point, Average Stats Point
 for line in open('pkmnstats.csv'):
     ind = line.split(',')
     ind[-1] = ind[-1].strip()
@@ -14,6 +16,8 @@ for line in open('pkmnstats.csv'):
     for i in range(3,11):
         pkmn[ind[0]][i] = float(pkmn[ind[0]][i])
 
+# Creates the 'tipo' dictionary. Each entrance contains information effectiveness of an attack. Each entrance is a vector with an "Effectiveness Modifier."
+# Also creates the 'tipoGuia' dictionary. Each entrance links a type name (e.g. 'Water') to an index number (e.g. 11), representing the location of the type in the 'guia' dictionary. Allows calculation of type effectiveness.
 for line in open('typechart.csv'):
     typ = line.split(',')
     tipo[typ[0]] = typ[1:]
@@ -22,69 +26,84 @@ for line in open('typechart.csv'):
     for i in range(0,18):
         tipo[typ[0]][i] = float(tipo[typ[0]][i])
         
-import random
+def battle(fighter1, fighter2):
+    "This function will be used to simulate the battle between 2 pokémon."
+
+    
+    # First, load the two dueling pokémon into the function and create blank effectiveness modifier for them.
+    pkmnA = pkmn[fighter1]
+    pkmnB = pkmn[fighter2]
+
+    modA = [1,1]
+    modB = [1,1]
+
+    # Modifier the effectiveness modifiers based on the types of the dueling pokémon and create index maximuns for checking types during the battles.
+    if pkmnA[2]=='':
+        iMax = 2
+        modA[1] = 0
+    else:
+        iMax = 3
 
 
-fighters = random.sample(list(pkmn.keys())[0:10],2)
-pkmnA = pkmn[fighters[0]]
-pkmnB = pkmn[fighters[1]]
-modA = [1,1]
-modB = [1,1]
-
-# just for fun
-
-# pkmnA = pkmn['26']
-# pkmnB = pkmn['9']
+    if pkmnB[2]=='':
+        jMax = 2
+        modB[1] = 0
+    else:
+        jMax = 3
 
 
-
-if pkmnA[2]=='':
-    iMax = 2
-    modA[1] = 0
-else:
-    iMax = 3
-
-
-if pkmnB[2]=='':
-    jMax = 2
-    modB[1] = 0
-else:
-    jMax = 3
-
-
-for i in range(1,iMax):
-    for j in range(1,jMax):
-        modA[i-1] *= tipo[pkmnA[i]][tipoGuia[pkmnB[j]]-1]
-
-for j in range(1,jMax):
     for i in range(1,iMax):
-        modB[j-1] *= tipo[pkmnB[j]][tipoGuia[pkmnA[i]]-1]
+        for j in range(1,jMax):
+            modA[i-1] *= tipo[pkmnA[i]][tipoGuia[pkmnB[j]]-1]
 
+    for j in range(1,jMax):
+        for i in range(1,iMax):
+            modB[j-1] *= tipo[pkmnB[j]][tipoGuia[pkmnA[i]]-1]
 
+    # The battle simulator itself. Runs until at least of the fighting pokémon is knocked down to 0 or below.
+    # It uses the type modifiers, compares Atk-Def and SpA-SpD and use the highest (strongest) option.
+    while pkmnB[3] >= 0 and pkmnA[3] >= 0:
+        damagePh = ((22 * 60 * (pkmnA[4]/pkmnB[5]))/50) * max(modA)
+        damageSp = ((22 * 60 * (pkmnA[6]/pkmnB[7]))/50) * max(modA)
 
-while pkmnB[3] >= 0 and pkmnA[3] >= 0:
-    damagePh = ((22 * 60 * (pkmnA[4]/pkmnB[5]))/50) * max(modA)
-    damageSp = ((22 * 60 * (pkmnA[6]/pkmnB[7]))/50) * max(modA)
+        if damagePh >= damageSp:
+            pkmnB[3] = pkmnB[3] - damagePh
+        else:
+            pkmnB[3] = pkmnB[3] - damageSp
 
-    if damagePh >= damageSp:
-        pkmnB[3] = pkmnB[3] - damagePh
+        damagePh = ((22 * 60 * (pkmnB[4]/pkmnA[5]))/50) * max(modB)
+        damageSp = ((22 * 60 * (pkmnB[6]/pkmnA[7]))/50) * max(modB)
+
+        if damagePh >= damageSp:
+            pkmnA[3] = pkmnA[3] - damagePh
+        else:
+            pkmnA[3] = pkmnA[3] - damageSp
+
+    if pkmnA[3] <= 0 and pkmnB[3] < 0:
+        if pkmnA[8] < pkmnB[8]:
+            winner = pkmnB[0]
+        else:
+            winner = pkmnA[0]
+
     else:
-        pkmnB[3] = pkmnB[3] - damageSp
-
-    damagePh = ((22 * 60 * (pkmnB[4]/pkmnA[5]))/50) * max(modB)
-    damageSp = ((22 * 60 * (pkmnB[6]/pkmnA[7]))/50) * max(modB)
-
-    if damagePh >= damageSp:
-        pkmnA[3] = pkmnA[3] - damagePh
-    else:
-        pkmnA[3] = pkmnA[3] - damageSp
+        if pkmnA[3] <= 0:
+            winner = pkmnB[0]
+        else:
+            winner = pkmnA[0]
+    
 
 
-print(pkmnA)
-print(pkmnB)
+    print(pkmnA)
+    print(pkmnB)
+    print('The winner is: ' + winner)
+    return
+
+import random
+fighters = random.sample(list(pkmn.keys())[0:10],2)
+
+battle(fighters[0], fighters[1])
 
 # amanha:
 # computar/tabelar as vitorias
-# add clausula pra qnd os dois caem pra menos de 0, maior speed vencer
 
     
